4a9d20533468aef2e4c3d83d5343924576555612
Aleks1y <74914340+Aleks1y@users.noreply.github.com>
2020-12-25 22:13:42 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 int main(int argc, char *argv[])
 {
+    bool inputFromWorkflow = 1,
+         outputFromWorkflow = 1;
     try
     {
         if (argc < 2)
@@ -24,24 +26,22 @@ int main(int argc, char *argv[])
                 {
                     wasInput = true;
                     input = argv[++i];
+                    inputFromWorkflow = 0;
                 }
                 else if (((string)"-o") == argv[i] && i != argc && !wasOutput)
                 {
                     wasOutput = true;
                     output = argv[++i];
+                    outputFromWorkflow = 0;
                 }
                 else
                     throw exception("Wrong args from prompt!");
             }
         }
 
-        Parser::parser(workflow, input, output).execute();
+        Parser::parser(workflow, input, output, inputFromWorkflow, outputFromWorkflow).execute();
     }
     catch (const char *str)
-    {
-        cout << str;
-    }
-    catch (string str)
     {
         cout << str;
     }
