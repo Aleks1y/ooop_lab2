847f132b9a227f9579e619dd7eed4fbfeda80f3a
Aleks1y <74914340+Aleks1y@users.noreply.github.com>
2021-01-04 23:26:53 +0700

    Update Header.h

--- Header.h
+++ Header.h
@@ -6,15 +6,14 @@
 #include <string>
 #include <algorithm>
 #include <list>
+#include <optional>
 
 using namespace std;
 
 class Worker
 {
 public:
-    bool isReader = 0;
-    bool isWriter = 0;
-    virtual void doWork(vector<string>& text) = 0;
+    virtual void doWork(optional<vector<string>>& text) = 0;
 };
 
 class FileReader : public Worker
@@ -22,8 +21,8 @@ class FileReader : public Worker
 private:
     string filename;
 public:
-    FileReader(string filename);
-    void doWork(vector<string>& text) override;
+    FileReader(string filename) : filename(filename) {};
+    void doWork(optional<vector<string>>& text) override;
 };
 
 class FileWriter : public Worker
@@ -31,8 +30,8 @@ class FileWriter : public Worker
 private:
     string filename;
 public:
-    FileWriter(string filename);
-    void doWork(vector<string>& text) override;
+    FileWriter(string filename) : filename(filename) {};
+    void doWork(optional<vector<string>>& text) override;
 };
 
 class Grep : public Worker
@@ -41,13 +40,13 @@ private:
     string word;
 public:
     Grep(string word) : word(word) {};
-    void doWork(vector<string>& text) override;
+    void doWork(optional<vector<string>>& text) override;
 };
 
 class Sort : public Worker
 {
 public:
-    void doWork(vector<string>& text) override;
+    void doWork(optional<vector<string>>& text) override;
 };
 
 class Replace : public Worker
@@ -57,7 +56,7 @@ private:
     string word2;
 public:
     Replace(string word1, string word2) : word1(word1), word2(word2) {};
-    void doWork(vector<string>& text) override;
+    void doWork(optional<vector<string>>& text) override;
 };
 
 class Dump : public Worker
@@ -66,7 +65,7 @@ private:
     string filename;
 public:
     Dump(string filename) : filename(filename) {};
-    void doWork(vector<string>& text) override;
+    void doWork(optional<vector<string>>& text) override;
 };
 
 class BlockProgram
@@ -77,7 +76,7 @@ private:
     string output;
 public:
     BlockProgram(list <shared_ptr<Worker>> queue, string input, string output) : queue(queue), input(input), output(output) {}
-    shared_ptr<Worker> BlocksParser(ifstream& fin);
+    shared_ptr<Worker> WorkerFabric(ifstream& fin);
     void execute();
 };
 
